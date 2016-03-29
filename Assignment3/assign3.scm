d57b44bd5d9e558fc9f651c45c4ea33cf3916329
Kyle Galloway <ckgalloway@crimson.ua.edu>
2016-03-29 16:40:00 -0500

    update

--- Assignment3/assign3.scm
+++ Assignment3/assign3.scm
@@ -15,69 +15,85 @@
 )
 
 ; Task 1
-
+(include "task1.scm")
 
 ; Task 2
-
+(include "task2.scm")
 
 ; Task 3
-
+(include "task3.scm")
 
 ; Task 4
-
+(include "task4.scm")
 
 ; Task 5
-
+(include "task5.scm")
 
 ; Task 6
-
+(include "task6.scm")
 
 ; Task 7
-
+(include "task7.scm")
 
 ; Task 8
-
+(include "task8.scm")
 
 ; Task 9
-
-
-; Tests
-; (define (run1)
-;
-; )
-; (define (run2)
-;
-; )
-; (define (run3)
-;
-; )
-; (define (run4)
-;
-; )
-; (define (run5)
-;
-; )
-; (define (run6)
-;
-; )
-; (define (run7)
-;
-; )
-; (define (run8)
-;
-; )
-; (define (run9)
-;
-; )
+(include "task9.scm")
 
 ; Run tests
-; (run1)
-; (run2)
-; (run3)
-; (run4)
-; (run5)
-; (run6)
-; (run7)
-; (run8)
-; (run9)
+(print "PROBLEM 1:\n\n")
+(if (defined? 'run1 this)
+    (run1)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 2:\n\n")
+(if (defined? 'run2 this)
+    (run2)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 3:\n\n")
+(if (defined? 'run3 this)
+    (run3)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 4:\n\n")
+(if (defined? 'run4 this)
+    (run4)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 5:\n\n")
+(if (defined? 'run5 this)
+    (run5)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 6:\n\n")
+(if (defined? 'run6 this)
+    (run6)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 7:\n\n")
+(if (defined? 'run7 this)
+    (run7)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 8:\n\n")
+(if (defined? 'run8 this)
+    (run8)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
+(print "PROBLEM 9:\n\n")
+(if (defined? 'run9 this)
+    (run9)
+    (println "\tNOT IMPLEMENTED")
+)
+(println "\n")
 (display "\n\nassignment 3 loaded!\n\n")
---
+++ Assignment3/task1.scm
@@ -0,0 +1,12 @@
+(define (level # id)
+    (car #)
+)
+
+; Tests
+(define (f x y)
+    (* (+ x y))
+)
+
+(define (run1)
+    (inspect (level 'f))
+)
