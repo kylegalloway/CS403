ea86654770dc29aa6392c9a0d83b38f2c1db0100
Kyle Galloway <ckgalloway@crimson.ua.edu>
2016-04-05 15:35:13 -0500

    update

--- Book Work/Ch2.scm
+++ Book Work/Ch2.scm
@@ -17,25 +17,34 @@
     )
 )
 
-; Rational Numbers
+; GCD
+(define (gcd a b)
+    (if (= b 0)
+        a
+        (gcd b (remainder a b))
+    )
+)
+
+; 2.1 Rational Numbers
 
 (define (make-rat n d)
+    (define g (gcd n d))
     (cond
         ((= d 0) (print "Error denom is 0"))
         ((and (= n 0))
-            (cons n d)
+            (cons (/ n g) (/ d g))
         )
         ((and (> n 0) (> d 0))
-            (cons n d)
+            (cons (/ n g) (/ d g))
         )
         ((and (> n 0) (< d 0))
-            (cons (- 0 n) (- 0 d))
+            (cons (/ (- 0 n) g) (/ (- 0 d) g))
         )
         ((and (< n 0) (> d 0))
-            (cons n d)
+            (cons (/ n g) (/ d g))
         )
         ((and (< n 0) (< d 0))
-            (cons (- 0 n) (- 0 d))
+            (cons (/ (- 0 n) g) (/ (- 0 d) g))
         )
     )
 )
@@ -93,6 +102,32 @@
     )
 )
 
+; Ex 2.2 Line Segments
+(define (make-segment a b) (cons a b))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (midpoint-segment s)
+    (define x1 (x-point (start-segment s)))
+    (define x2 (x-point (end-segment s)))
+    (define y1 (y-point (start-segment s)))
+    (define y2 (y-point (end-segment s)))
+    (make-point (- x2 x1) (- y2 y1))
+)
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")")
+  (newline)
+)
+
 ; Tests
 (define a (make-rat 1 2))
 (define b (make-rat 3 4))
@@ -106,3 +141,13 @@
 ; (exprTest c (cons -1 2))
 ; (exprTest d (cons -1 2))
 ; (exprTest e (cons 1 2))
+
+; Tests reduced make-value
+; (exprTest (mult-rat a b) (cons 3 8))
+; (exprTest (div-rat a b) (cons 2 3))
+; (exprTest (div-rat b a) (cons 3 2))
+
+(define a (make-point 1 1))
+(define b (make-point 3 3))
+(define s (make-segment a b))
+(print-point (midpoint-segment s))
