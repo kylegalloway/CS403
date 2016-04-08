4c686df46ec4a4954be127e102c23681a1b957d3
Kyle Galloway <ckgalloway@crimson.ua.edu>
2016-04-08 12:16:11 -0500

    update

---
+++ Assignment3/task3.scm
@@ -0,0 +1,26 @@
+(define (item v r)
+    (define (dispatch m)
+        (cond
+            ((eq? m 'val) v)
+            ((eq? m 'rank) r)
+            (else (error "Unknown operation -- Item" m))
+        )
+    )
+    dispatch
+)
+
+(define (PRQ comp)
+    (define queue '())
+    (define (dispatch m . args)
+        (cond
+            ((eq? m 'insert) (append queue (item (car args) (cadr args))))
+            ((eq? m 'remove) (define pop (car queue)) (set! queue (cdr queue)))
+            ((eq? m 'item) ((car queue) 'val))
+            ((eq? m 'rank) ((car queue) 'rank))
+            ((eq? m 'size) (length queue))
+            ((eq? m 'empty?) (= (length queue) 0))
+            (else (error "Unknown operation -- PriorityQueue" m))
+        )
+    )
+    dispatch
+)
