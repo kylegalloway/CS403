dadadecfc9d2b9ebc5b542004097a76851effd25
Kyle Galloway <kyle.galloway.1993@gmail.com>
2016-04-18 22:32:50 -0500

    update

--- Assignment3/tasks/task2.scm
+++ Assignment3/tasks/task2.scm
@@ -1,22 +1,67 @@
 (include "extras/exprTest.scm")
 (include "pretty.lib")
 
-(define (denv id) ((get'__context) id))
+(define (denv f) (get'__context f))
 
-(define local-ids (get 'parameters f))
+(define (included? x L)
+    (if (null? L)
+        #f
+        (begin
+            (if (== (car L) x)
+                #t
+                (included? x (cdr L))
+            )
+        )
+    )
+)
 
-()
+(define (cache f)
+    ; capture the local variables (parameters)
+    (define local-ids (get 'parameters f))
+    ; iterate through the code
+    (define (iter expr)
+        (cond
+            ; if the expr is a pair, and the car is define
+            ; add the defined id to the local-ids list
+            ; and iterate through the cddr to keep going
+            ((pair? expr)
+                (if (eq? (car expr) 'define)
+                    (begin
+                        (if (pair? (cadr expr))
+                            (set! local-ids (append (cadr expr) local-ids))
+                            (set! local-ids (cons (cadr expr) local-ids))
+                        )
+                        (inspect local-ids)
+                        (cons 'define (cons (cadr expr) (iter (cddr expr))))
+                    )
+                    (cons (iter (car expr)) (iter (cdr expr)))
+                )
+            )
+            ((included? expr local-ids) expr)
+            (else (eval expr (denv f)))
+        )
+    )
+    (set* f'code (cons 'begin (cdr (iter (get 'code f)))))
+)
 
-; (define (cache x)
-
-; )
-
-; (define (run2)
-;     (define m 2)
-;     (define (msquare x) (* m x x))
-;     ; (pretty msquare)
-;     (define denv (get '__context msquare))
-;     (ppTable denv)
-;     (cache msquare)
-;     ; (pretty msquare)
-; )
+(define (run2)
+    (define m 2)
+    (define (msquare x) (* m x x))
+    (pretty msquare)
+    (cache msquare)
+    (pretty msquare)
+    (newline)
+    (newline)
+    (define a 1)
+    (define b 2)
+    (define (f x y)
+        (define c 3)
+        (define (g)
+            (+ a b c x y)
+        )
+        (g)
+    )
+    (pretty f)
+    (cache f)
+    (pretty f)
+)
